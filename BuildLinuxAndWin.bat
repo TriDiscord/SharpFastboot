48cb309eda032bb5239c4d5b71ec8c19a3bb8ef3
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-12-30 12:02:26 +0000

    Update BuildLinuxAndWin.bat
    This will make the exe way bigger but it’s defo worth it for better support

--- BuildLinuxAndWin.bat
+++ BuildLinuxAndWin.bat
@@ -1,2 +1,2 @@
-dotnet publish --output build\ --framework net6.0 --arch x64 --os linux -c release --self-contained false && dotnet publish --output build\ --framework net6.0 --arch x86 --os win -c release --self-contained false
+dotnet publish --output build\ --framework net6.0 --arch x64 --os linux -c release --self-contained true && dotnet publish --output build\ --framework net6.0 --arch x86 --os win -c release --self-contained true
 
